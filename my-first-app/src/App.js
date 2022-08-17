88796ce9172a0bceb0feac6604ad52eb3cb3dc26
Hamza Belovodjanin <hbelovodjanin@gmail.com>
2022-08-17 19:33:24 +0200

    react

--- my-first-app/src/App.js
+++ my-first-app/src/App.js
@@ -1,23 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./App.css"
 
 export default function App() {
 
-const [name,setName] = React.useState("");
-const [email,setEmail] = React.useState("")
-const [hobi,setHobi] = React.useState("")
+// const [name,setName] = React.useState("");
+// const [email,setEmail] = React.useState("")
+// const [hobi,setHobi] = React.useState("")
+
+const [formValues, setFormValues] = useState ({
+  name: "",
+  email: "",
+  hobi: "",
+  password: "",
+});
 
   return (
 <div className="card-container">
       <form onSubmit={(e) => {e.preventDefault()
-      console.log(`NAME: ${name} EMAIL: ${email} HOBI ${hobi} `)}}>
+      console.log(formValues)
+      console.log(`NAME: ${formValues.name} EMAIL: ${formValues.email} HOBI ${formValues.hobi} `)}}>
         <label htmlFor="html">Name</label>
         <input 
           type="text" 
           id="html" 
           name="fav_language"  
-          value={name}
-          onChange={(event) => setName(event.target.value)}/>
+          value={formValues.name}
+          onChange={(event) => setFormValues(prev => ({
+            ...prev,
+            name: event.target.value
+          }))}/>
         <br />
 
         <label htmlFor="css">email</label>
@@ -25,8 +36,11 @@ const [hobi,setHobi] = React.useState("")
         type="text" 
         id="css" 
         name="fav_language"
-        value={email}
-        onChange={(event) => setEmail(event.target.value)}/>
+        value={formValues.email}
+        onChange={(event) => setFormValues(prev => ({
+          ...prev,
+          email: event.target.value
+        }))}/>
         <br />
 
 
@@ -35,17 +49,20 @@ const [hobi,setHobi] = React.useState("")
         type="text" 
         id="javascript"
          name="fav_language"
-        value={hobi}
-        onChange={(event) => setHobi(event.target.value)}/>
+        value={formValues.hobi}
+        onChange={(event) => setFormValues(prev => ({
+          ...prev,
+          hobi: event.target.value
+        }))}/>
         <br />
         <br />
         <input type="submit" defaultValue="Submit" />
       </form>
-    </div>
+
+      
+
+
+      
+</div>
   )
-
-
-}
-
-
-
+      }
